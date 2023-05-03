7ffd86054d24fb21027bf3e38b6ac9a21d805c2a
gyujin_choi <gyujin_choi@tmax.co.kr>
2023-05-03 09:15:54 +0900

    cm - tmax_1

--- examples/effective_java/src/main/java/study/prosync/ch2/Elvis.java
+++ examples/effective_java/src/main/java/study/prosync/ch2/Elvis.java
@@ -1,6 +1,5 @@
 package study.prosync.ch2;
 
-import java.io.Serializable;
 
 public class Elvis {
   public static final Elvis INSTANCE = new Elvis();
--- examples/effective_java/src/main/java/study/prosync/ch2/test1.java
+++
@@ -1,5 +0,0 @@
-package study.prosync.ch2;
-
-public class test1 {
-
-}
